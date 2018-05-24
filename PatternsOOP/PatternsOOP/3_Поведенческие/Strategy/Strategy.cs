11ede8545607c193998d5ec11d3a648129013213
suleimanovrf <rasim_suleimanov@mail.ru>
2018-05-24 15:31:04 +0300

    Добавляем паттерн стратегия.

---
+++ PatternsOOP/PatternsOOP/3_Поведенческие/Strategy/Strategy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsOOP._3_Поведенческие.Strategy
+{
+	public interface IDrink
+	{
+		void Open();
+	}
+
+	public class Pepsi : IDrink
+	{
+		public void Open()
+		{
+			Console.WriteLine("Откручиваем крышку по часовой стрелке!!!");
+		}
+	}
+	public class CocaCola : IDrink
+	{
+		public void Open()
+		{
+			Console.WriteLine("Откручиваем крышку против часовой стрелке!!!");
+		}
+	}
+	public class People
+	{
+		IDrink drink;
+		public People(IDrink drink)
+		{
+			this.drink = drink;
+		}
+		public void Drink()
+		{
+			drink.Open();
+		}
+	}
+	class ClientStrategy
+	{
+		public void Run()
+		{
+			People people = new People(new CocaCola());
+			people.Drink();
+
+			people = new People(new Pepsi());
+			people.Drink();
+		}
+	}
+}
