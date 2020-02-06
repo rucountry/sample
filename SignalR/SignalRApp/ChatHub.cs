49863d0da54844a55df8c87f4b1c9fc48257ba5f
rasim_home <rasim_suleimanov@mail.ru>
2020-02-06 22:35:03 +0300

    SignalR sample

---
+++ SignalR/SignalRApp/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApp {
+	public class ChatHub : Hub {
+		public async Task Send(string message, string userName) {
+			// This command 
+			await this.Clients.All.SendAsync("Send", message, userName);
+		}
+	}
+}
